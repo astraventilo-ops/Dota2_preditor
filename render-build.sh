f8018539a2c609f447ffd30ad8f64afd0c9ee84b
astraventilo-ops <astraventilo@gmail.com>
2026-08-10 17:34:21 +0000

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-# Installation des dépendances
+# 1. Mise à jour de pip et installation des packages Python
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Installation des navigateurs pour Playwright
-playwright install chromium
-playwright install-deps chromium
+# 2. Installation de Chromium uniquement (sans privilèges root/sudo)
+python -m playwright install chromium
